9cbca6b03de4499c6e5271868cd5f6889daa1472
SocialSupaCrew <socialsupacrew@gmail.com>
2015-12-13 20:38:33 +0100

    delete secret santa page

--- secretsanta/js/app.js
+++
@@ -1,9 +0,0 @@
-var app = angular.module('cadeauxNoel', ['ngRoute', 'ngAnimate', 'ngMaterial'])
-
-.config(function($routeProvider) {
-	$routeProvider
-
-	.when('/', {templateUrl: 'template/index.html', controller: 'cadeauxCtrl'})
-
-	.otherwise({redirectTo: '/'});
-});
